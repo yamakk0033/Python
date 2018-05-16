946708adc7ad87a56409113db84f452044af12b3
test_user <testuser00990099@outlook.jp>
2018-05-17 06:22:18 +0900

    ツール作成中

--- JanKenPon/LineInfo.py
+++ JanKenPon/LineInfo.py
@@ -5,20 +5,45 @@ Created on Tue May 15 21:20:40 2018
 @author: Tanak
 """
 
-import sys
 import pygame
 import pickle
 import glob
 
 
 
-aabbcc = 444
-
 class LineInfo:
     def __init__(self, depth, x, y):
         self.depth = depth
         self.x = x
         self.y = y
+
+
+def DrawLines(screen, array):
+    buff = []
+    for info in array:
+        buff.append((info.x, info.y))
+
+    if len(buff) > 1:
+        print("aa")
+        pygame.draw.lines(screen, (255, 255, 255), False, buff)
+    elif len(buff) == 1:
+        print("bb")
+        pygame.draw.circle(screen, (255, 255, 255), buff[0], 5)
+
+
+def DrawLinesEx(screen, array):
+    prev = 0
+    buff = []
+
+    for info in array:
+        if info.depth != prev:
+            DrawLines(screen, buff)
+            buff = []
+        buff.append(info)
+        prev = info.depth
+
+    DrawLines(screen, buff)
+
 
 
 def OutputData(name, list):
@@ -32,3 +57,13 @@ def InputData(name):
         list = pickle.load(f)
 
     return list
+
+
+
+def GetFiles(name):
+    return glob.glob(name)
+
+
+
+
+
--- JanKenPon/test.py
+++ JanKenPon/test.py
@@ -1,4 +1,5 @@
-﻿import sys
+﻿
+import sys
 import pygame
 import LineInfo
 from pygame.locals import QUIT, MOUSEBUTTONDOWN
@@ -6,6 +7,7 @@ from pygame.locals import QUIT, MOUSEBUTTONDOWN
 
 SCREEN_WIDTH = 624
 SCREEN_HEIGHT = 816
+
 
 def main():
     pygame.init()
@@ -16,46 +18,70 @@ def main():
     SURFACE = pygame.display.set_mode((SCREEN_WIDTH + 192, SCREEN_HEIGHT))
     FPSCLOCK = pygame.time.Clock()
 
-    sysfont = pygame.font.SysFont(None, 36)
 
+    depth = 0
     mousepos = []
+    fileName = ""
+
 
     readRect  = pygame.Rect(644,  20, 152, 100)
     writeRect = pygame.Rect(644, 140, 152, 100)
-    read_image = sysfont.render("read", True, (200, 200, 200))
-    write_image = sysfont.render("write", True, (200, 200, 200))
+
+    sysfont = pygame.font.SysFont(None, 36)
+    readImage  = sysfont.render("read",  True, (200, 200, 200))
+    writeImage = sysfont.render("write", True, (200, 200, 200))
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == MOUSEBUTTONDOWN:
-                if event.pos[0] in range(0, SCREEN_WIDTH) and event.pos[1] in range(0, SCREEN_HEIGHT):
-                    mousepos.append(event.pos)
-                    
-                
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
 
+                if x in range(0, SCREEN_WIDTH) and y in range(0, SCREEN_HEIGHT):
+                    mousepos.append(LineInfo.LineInfo(depth, x, y))
+
+                elif x in range(readRect.left, readRect.right) and y in range(readRect.top, readRect.bottom):
+
+                    if fileName == "":
+                        files = LineInfo.GetFiles(fileName)
+                        if len(files) > 1:
+                            fileName = files[0]
+
+                    mousepos = LineInfo.InputData(fileName)
+
+                elif x in range(writeRect.left, writeRect.right) and y in range(writeRect.top, writeRect.bottom):
+
+                    if fileName == "":
+                        fileName = "test/aa.dat"
+
+                    LineInfo.OutputData(fileName, mousepos)
+
+
+            elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+                depth += 1
 
         SURFACE.fill((0, 0, 0))
 
-        pygame.draw.line(SURFACE, (255, 255, 255), (624, 0), (624, 816), 1)
+        pygame.draw.line(SURFACE, (255, 255, 255), (SCREEN_WIDTH, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), 1)
         pygame.draw.rect(SURFACE, (255, 255, 255), readRect)
         pygame.draw.rect(SURFACE, (255, 255, 255), writeRect)
 
-        SURFACE.blit(read_image,  (664, 40))
-        SURFACE.blit(write_image, (664, 160))
+        SURFACE.blit(readImage,  (664, 40))
+        SURFACE.blit(writeImage, (664, 160))
 
-        for i, j in mousepos:
-            pygame.draw.circle(SURFACE, (0, 255, 0), (i, j), 5)
-
+        LineInfo.DrawLinesEx(SURFACE, mousepos)
+        #for info in mousepos:
+        #    pygame.draw.circle(SURFACE, (0, 255, 0), (info.x, info.y), 5)
 
         pygame.display.update()
-        FPSCLOCK.tick(30)
+        FPSCLOCK.tick(5)
 
 
 if __name__ == "__main__":
     main()
+
 
 
 
